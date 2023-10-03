9beaf6c913a3b963312520e8d29e66403b7b1f9f
Pauagustimontull <pauagustimontull@gmail.com>
2023-10-03 20:05:02 +0200

    Proyecto java dentro
    Java

---
+++ BOOK4U/src/book4u/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book4u;
+
+/**
+ *
+ * @author Pau
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Marco newmarco = new Marco();
+    }
+    
+}
